7c5a1deb8884c9f9820eff00068c9e6cc03055d2
philiphogfeldt <philipehogfeldt@hotmail.se>
2023-11-01 11:24:36 +0100

    mappers per day klar

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,7 +1,9 @@
 package com.plcoding.weatherapp.data.mappers
 
 import com.plcoding.weatherapp.data.remote.WeatherDataDto
+import com.plcoding.weatherapp.data.remote.WeatherDto
 import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -38,4 +40,17 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int,List<WeatherData>> {
     }
     //Kan läggas för att listorna ska skrivas ut
     //.also {println(it.Key)} eller it.values
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo{
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0].find {
+        val hour = if (now.minute < 30) now.hour else now.hour + 1
+        it.time.hour == hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
 }
