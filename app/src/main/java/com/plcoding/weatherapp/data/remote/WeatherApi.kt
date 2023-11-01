cdea91ea327e720af98a1c12ae2e8bea52c3843c
philiphogfeldt <philipehogfeldt@hotmail.se>
2023-11-01 11:19:38 +0100

    Api klart, jobbar med mappers

--- app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherApi.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherApi.kt
@@ -6,7 +6,13 @@ import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("https://api.open-meteo.com/v1/forecast?latitude=57.721&longitude=12.9401&hourly=temperature_2m")
+    @GET("https://api.open-meteo.com/v1/forecast?" +
+            "latitude=57.721" +
+            "&longitude=12.9401" +
+            "&current=weathercode" +
+            "&hourly=temperature_2m," +
+            "surface_pressure," +
+            "windspeed_10m")
     suspend fun  getWeatherData(
         @Query("latitude") lat: Double,
         @Query("longitude") long: Double
